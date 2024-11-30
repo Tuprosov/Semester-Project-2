cbddc920083e7a2f2f63a2c31e49c40697b4261a
Shahzod <shahzodwill@gmail.com>
2024-11-30 15:15:18 +0100

    add new files

---
+++ src/js/constants.js
@@ -0,0 +1,11 @@
+export const API_KEY = "9b9279a4-5aae-4428-bb34-ce6cfb24d805";
+
+export const API_BASE = "https://v2.api.noroff.dev";
+
+export const API_AUTH = `${API_BASE}/auth`;
+
+export const API_AUTH_LOGIN = `${API_AUTH}/login`;
+
+export const API_AUTH_REGISTER = `${API_AUTH}/register`;
+
+export const API_SOCIAL_PROFILES = `${API_SOCIAL}/profiles`;
