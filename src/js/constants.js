6e2fecad347f1675e1f7fbbf571e5ac20456a389
Shahzod <shahzodwill@gmail.com>
2024-12-01 12:15:33 +0100

    create constants for api

--- src/js/constants.js
+++ src/js/constants.js
@@ -7,5 +7,3 @@ export const API_AUTH = `${API_BASE}/auth`;
 export const API_AUTH_LOGIN = `${API_AUTH}/login`;
 
 export const API_AUTH_REGISTER = `${API_AUTH}/register`;
-
-export const API_SOCIAL_PROFILES = `${API_SOCIAL}/profiles`;
