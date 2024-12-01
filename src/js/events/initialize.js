41445c8c1e36b86272d34e27476d321c0da9b5bf
Shahzod <shahzodwill@gmail.com>
2024-12-01 12:14:39 +0100

    create a function to init events

--- src/js/events/initialize.js
+++ src/js/events/initialize.js
@@ -1,1 +1,8 @@
 // import all event handlers here
+import { loginEvent, registerEvent, logoutEvent } from "./auth.js";
+
+export function initializeEvents() {
+  registerEvent();
+  loginEvent();
+  logoutEvent();
+}
