e343e5fee928196aec2dbf970c0c3fbb48c63fc1
Shahzod <shahzodwill@gmail.com>
2024-12-16 16:57:52 +0100

    import router to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { initializeEvents } from "./js/events/initialize.js";
 import { checkStatus } from "./js/utils/statusCheck.js";
+import router from "./js/router.js";
 
 initializeEvents();
 checkStatus();
+await router();
