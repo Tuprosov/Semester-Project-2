e86e2cdb40afd7d633c61ec26fb4d1a3f3344ca5
Shahzod <shahzodwill@gmail.com>
2024-12-16 13:56:18 +0100

    change location of index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { initializeEvents } from "./js/events/initialize.js";
+import { checkStatus } from "./js/utils/statusCheck.js";
+
+initializeEvents();
+checkStatus();
--- src/js/index.js
+++
@@ -1,5 +0,0 @@
-import { initializeEvents } from "./events/initialize.js";
-import { checkStatus } from "./utils/statusCheck.js";
-
-initializeEvents();
-checkStatus();
