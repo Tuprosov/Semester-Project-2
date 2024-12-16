4ad1389bd0be310e7c244c10015f0a45f0985ce5
Shahzod <shahzodwill@gmail.com>
2024-12-16 11:15:30 +0100

    add method to user class

--- src/js/classes/user.js
+++ src/js/classes/user.js
@@ -1,10 +1,33 @@
 export class User {
-  constructor(username, email, avatar = null, credits = 1000) {
+  constructor(
+    username,
+    email,
+    avatar = null,
+    alt = null,
+    credits = 1000,
+    token
+  ) {
     this.username = username;
     this.email = email;
     this.avatar = avatar;
+    this.alt = alt;
     this.credits = credits;
+    this.token = token;
     this.bids = []; // An array to store bids made by the user
     this.watchlist = []; // An array to store the listings the user is watching
+  }
+
+  static get loggedUser() {
+    const userData = localStorage.getItem("loggedUser");
+    return userData ? JSON.parse(userData) : null;
+  }
+
+  static isLoggedIn() {
+    const loggedUser = this.loggedUser;
+    return loggedUser && loggedUser.token; // Returns true if loggedUser exists and has a token
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem("loggedUser", JSON.stringify(this));
   }
 }
