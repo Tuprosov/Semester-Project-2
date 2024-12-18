3808986ea4a8d85e90d5d467ddef9df3d4d79208
Shahzod <shahzodwill@gmail.com>
2024-12-18 17:17:13 +0100

    complete pagination

--- src/js/ui/home.js
+++ src/js/ui/home.js
@@ -1,7 +1,13 @@
-import { API } from "../classes/api";
-import { API_BASE } from "../constants";
-import { displayListings } from "../utils/render";
+import { API } from "../classes/api.js";
+import { API_BASE } from "../constants.js";
+import { displayListings, pagination } from "../utils/render.js";
 
-const api = new API(API_BASE);
-const listings = api.getListings();
-displayListings(listings);
+export async function loadListings(page = 1) {
+  const api = new API(API_BASE);
+  const limit = 12;
+  const listings = await api.getListings(limit, page);
+  displayListings(listings.data);
+  pagination(listings.meta);
+}
+
+await loadListings();
