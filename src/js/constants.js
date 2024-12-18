5c99474eb480b6b939d255436d516088d16e8724
Shahzod <shahzodwill@gmail.com>
2024-12-18 17:18:22 +0100

    change api base link for auctions

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,6 +1,6 @@
 export const API_KEY = "9b9279a4-5aae-4428-bb34-ce6cfb24d805";
 
-export const API_BASE = "https://v2.api.noroff.dev";
+export const API_BASE = "https://v2.api.noroff.dev/auction/listings";
 
 export const API_AUTH = `${API_BASE}/auth`;
 
