b58bac3a6041c0fd625b405497b7704185686713
Shahzod <shahzodwill@gmail.com>
2024-12-18 17:17:39 +0100

    add event for search

--- src/js/events/initialize.js
+++ src/js/events/initialize.js
@@ -1,8 +1,14 @@
 // import all event handlers here
 import { loginEvent, registerEvent, logoutEvent } from "./auth.js";
+import { performSearch } from "../ui/search.js";
 
 export function initializeEvents() {
   registerEvent();
   loginEvent();
   logoutEvent();
+  document
+    .getElementById("searchInput")
+    .addEventListener("keypress", (event) => {
+      if (event.key === "Enter") performSearch();
+    });
 }
