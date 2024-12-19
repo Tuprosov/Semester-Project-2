ab51484ebffc2f69372339f3953fa282ae06f006
Shahzod <shahzodwill@gmail.com>
2024-12-19 14:40:56 +0100

    add api noroff base as constant

--- src/js/constants.js
+++ src/js/constants.js
@@ -2,10 +2,12 @@ export const API_KEY = "9b9279a4-5aae-4428-bb34-ce6cfb24d805";
 
 export const API_BASE = "https://v2.api.noroff.dev/auction/listings";
 
-export const API_AUTH = `${API_BASE}/auth`;
+export const API_NOROFF_BASE = "https://v2.api.noroff.dev";
+
+export const API_AUTH = `${API_NOROFF_BASE}/auth`;
 
 export const API_AUTH_LOGIN = `${API_AUTH}/login`;
 
 export const API_AUTH_REGISTER = `${API_AUTH}/register`;
 
-export const base = "/Semester-Project-2";
+export const GITHUB_BASE = "/Semester-Project-2";
