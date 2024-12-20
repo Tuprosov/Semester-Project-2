9d2a33394946cce2544e02327b4921e9d7430258
Shahzod <shahzodwill@gmail.com>
2024-12-20 10:30:28 +0100

    update index.js accordingly

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import { initializeEvents } from "./js/events/initialize.js";
 import { checkStatus } from "./js/utils/statusCheck.js";
 import router from "./js/router.js";
+import { displayUserDetails } from "./js/utils/displayUser.js";
+import { User } from "./js/classes/user.js";
 
 initializeEvents();
-checkStatus();
+if (checkStatus()) {
+  displayUserDetails(User.loggedUser);
+}
 await router();
