05eae8618bfe764ebf9dfebefac1ba13d1b55875
Shahzod <shahzodwill@gmail.com>
2024-12-20 10:30:00 +0100

    add case in router

--- src/js/router.js
+++ src/js/router.js
@@ -1,14 +1,19 @@
 import { loadListings } from "./ui/home.js";
+import { getClickedListing, toggleBidSection } from "./ui/listing.js";
+import { initializeSlider } from "./utils/carousel.js";
 
 export default async function router(pathname = window.location.pathname) {
-  console.log("router running");
   switch (pathname) {
     // case base:
     // case `${base}/index.html`:
     case "/index.html":
       await loadListings();
       break;
-
+    case "/listing/index.html":
+      await getClickedListing();
+      initializeSlider();
+      toggleBidSection();
+      break;
     default:
       break;
   }
