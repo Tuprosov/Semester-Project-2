ce4bdff4d966308b3c71f8c0f3cf74e688008234
Shahzod <shahzodwill@gmail.com>
2024-12-20 12:17:06 +0100

    corrected auth header

--- src/js/utils/headers.js
+++ src/js/utils/headers.js
@@ -1,14 +1,17 @@
 import { API_KEY } from "../constants.js";
+import { User } from "../classes/user.js";
 
 export function headers() {
   const headers = new Headers();
+  const currentUser = User.loggedUser;
+  console.log(currentUser.token);
 
   if (API_KEY) {
     headers.append("X-Noroff-API-Key", API_KEY);
   }
 
-  if (localStorage.token) {
-    headers.append("Authorization", `Bearer ${localStorage.token}`);
+  if (currentUser.token) {
+    headers.append("Authorization", `Bearer ${currentUser.token}`);
   }
 
   headers.append("Content-type", "application/json");
