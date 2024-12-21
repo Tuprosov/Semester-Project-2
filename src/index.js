c908f2357297b6fb157669766212b4aafbf21663
Shahzod <shahzodwill@gmail.com>
2024-12-21 14:24:27 +0100

    change function name

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { initializeEvents } from "./js/events/initialize.js";
 import { checkStatus } from "./js/utils/statusCheck.js";
 import router from "./js/router.js";
-import { displayUserDetails } from "./js/utils/displayUser.js";
+import { displayHeaderDetails } from "./js/utils/render.js";
 import { User } from "./js/classes/user.js";
 
 initializeEvents();
 if (checkStatus()) {
-  displayUserDetails(User.loggedUser);
+  displayHeaderDetails(User.loggedUser);
 }
 await router();
