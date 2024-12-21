d1cd9b9ee750d4b7187daf621d3556a1264d9f85
Shahzod <shahzodwill@gmail.com>
2024-12-21 14:22:45 +0100

    correct headers function

--- src/js/utils/headers.js
+++ src/js/utils/headers.js
@@ -1,17 +1,15 @@
 import { API_KEY } from "../constants.js";
-import { User } from "../classes/user.js";
 
 export function headers() {
   const headers = new Headers();
-  const currentUser = User.loggedUser;
-  console.log(currentUser.token);
+  const token = JSON.parse(localStorage.getItem("token"));
 
   if (API_KEY) {
     headers.append("X-Noroff-API-Key", API_KEY);
   }
 
-  if (currentUser.token) {
-    headers.append("Authorization", `Bearer ${currentUser.token}`);
+  if (token) {
+    headers.append("Authorization", `Bearer ${token}`);
   }
 
   headers.append("Content-type", "application/json");
