37ef404d20dc8465563ade59fc5826f32af10ccd
Shahzod <shahzodwill@gmail.com>
2024-12-22 15:58:32 +0100

    update onupdateAccount fn

--- src/js/ui/account.js
+++ src/js/ui/account.js
@@ -29,21 +29,50 @@ export async function onUpdateAccount() {
   const api = new API(API_PROFILE_BASE);
   const name = User.loggedUser.username;
   const token = JSON.parse(localStorage.getItem("token"));
-  const profile = await api.updateAccount(name);
+  const avatarUrl = document.getElementById("avatarUrl").value;
+  const bio = document.getElementById("bio");
+  const bannerUrl = document.getElementById("banner");
+  const message = document.getElementById("errorMessage");
+  const body = {};
 
-  //   update loggeduser object with updated avatar
-  const loggedUser = new User(
-    profile.name,
-    profile.email,
-    profile.avatar.url,
-    profile.avatar.alt,
-    token,
-    profile.credits,
-    profile.bio,
-    profile.listings,
-    profile.wins,
-    profile.bids,
-    profile.watchlist
-  );
-  loggedUser.saveToLocalStorage();
+  // form body object
+  if (avatarUrl) {
+    avatarUrl.trim();
+    body.avatar = { url: avatarUrl, alt: "" };
+  }
+  if (bio) {
+    body.bio = bio.value;
+  }
+  if (bannerUrl) {
+    bannerUrl.value.trim();
+    body.banner = { url: bannerUrl, alt: "" };
+  }
+
+  try {
+    const profile = await api.updateAccount(name, body);
+    message.textContent = "Avatar updated";
+    message.classList.remove("hidden");
+
+    //   update loggeduser object with updated avatar
+    const loggedUser = new User(
+      profile.name,
+      profile.email,
+      profile.avatar.url,
+      profile.avatar.alt,
+      token,
+      profile.credits,
+      profile.bio,
+      profile.listings,
+      profile.wins,
+      profile.bids,
+      profile.watchlist
+    );
+    loggedUser.saveToLocalStorage();
+
+    // Refresh the page after the update
+    location.reload();
+  } catch (error) {
+    message.textContent = error;
+    message.classList.remove("hidden");
+  }
 }
